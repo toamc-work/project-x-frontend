699a01c608945b58b4158c343d16f58b39ce62a3
toamc-work <toamc.work@gmail.com>
2025-01-24 02:19:11 +0200

    setup basic temp application theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 
+import { ThemeProvider as MuiThemeProvider, CssBaseline } from "@mui/material";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { muiTheme } from "./theme";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MuiThemeProvider theme={muiTheme}>
+      <StyledThemeProvider theme={muiTheme}>
+        <CssBaseline />
+        <App />
+      </StyledThemeProvider>
+    </MuiThemeProvider>
   </StrictMode>,
 );
